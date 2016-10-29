e01b5508fc1a7f952df31c2743df55c06ff2281e
Daniela Plascencia <daplascen@gmail.com>
2016-10-29 23:43:24 +0000

    modifies script

--- bluscript.sh
+++ bluscript.sh
@@ -2,23 +2,14 @@
 
 # Enable Bluetooth
 /usr/sbin/rfkill unblock bluetooth
-/usr/bin/hciconfig hci0 up
-
-## Enable default-agent
-bluetoothctl << EOF
-power on
-discoverable on
-pairable on
-quit
-EOF
-sleep 1
 
 # Enable simple agent
-python /home/root/edison_b/simple-agent1 &
-sleep 1
+#python /home/root/edison_b/simple-agent1 &
+#sleep 1
 
 # Enable SPP
 python /home/root/edison_b/spp.py &
+sleep 1
 ## Ask the user for the MAC address of device
 #echo "Write the MAC addres you want to connect to and press [ENTER]"
 #read mac_address
--- spp.py
+++ spp.py
@@ -11,6 +11,21 @@ BUS_NAME = 'org.bluez'
 AGENT_INTERFACE = 'org.bluez.Agent1'
 AGENT_PATH = "/test/agent"
 
+bus = None
+device_obj = None
+dev_path = None
+
+def set_trusted(path):
+    props = dbus.Interface(bus.get_object("org.bluez", path), "org.freedesktop.DBus.Properties")
+    props.Set("org.bluez.Device1", "Trusted", True)
+
+class Agent(dbus.service.Object):
+    @dbus.service.method(AGENT_INTERFACE, in_signature="ou", out_signature="")
+    def RequestConfirmation(self, device, passkey):
+        print("\nEnsure this passkey matches with the one in your device: %06d" % passkey)
+        set_trusted(device)
+        return
+
 class Profile(dbus.service.Object):
 	fd = -1
 	@dbus.service.method("org.bluez.Profile1",
@@ -25,28 +40,36 @@ class Profile(dbus.service.Object):
 		try:
 		    while True:
 		        data = server_sock.recv(1024)
-		        print("Smartphone says: %s" % data)
-			server_sock.send("Edison received: %s\n" % data)
+		        #print("Smartphone says: %s" % data)
+			#server_sock.send("Edison received: %s" % data)
 		except IOError:
 		    pass
 
 		server_sock.close()
-		print("Disconnected")
+		print("\nDisconnected")
 
 if __name__ == '__main__':
         
         # Generic dbus config
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 	bus = dbus.SystemBus()
+        obj = bus.get_object(BUS_NAME, "/org/bluez");
 
-        #Profile config
-	profile_manager = dbus.Interface(bus.get_object("org.bluez", "/org/bluez"), "org.bluez.ProfileManager1")	        
+        # Profile config
+	profile_manager = dbus.Interface(obj, "org.bluez.ProfileManager1")
         profile_path = "/foo/bar/profile"
         auto_connect = {"AutoConnect": False}
         profile_uuid = "1101"
 	profile = Profile(bus, profile_path)
 	profile_manager.RegisterProfile(profile_path, profile_uuid, auto_connect)
-   
+        # Agent config
+        agent_capability = "KeyboardDisplay"
+        agent_path = "/test/agent"
+        agent = Agent(bus, agent_path)
+        agent_manager = dbus.Interface(obj, "org.bluez.AgentManager1")
+        agent_manager.RegisterAgent(agent_path, agent_capability)
+        agent_manager.RequestDefaultAgent(agent_path)
+
         # Mainloop
 	mainloop = GObject.MainLoop()
 	mainloop.run()
