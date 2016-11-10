0452e00596a3c02401a6ddc811739210627c1e02
Jair Gonzalez <jairdeje@jairdeje-mobl1.amr.corp.intel.com>
2016-11-09 22:03:49 -0600

    projects/hospital_system: Adding project hospital_system
    Files included:
     spp.py
     actions.py
    
    For this project spp.py had to be modified to use a thread for handling
    Bluetooth socket events to allow for button input to be processed in
    realtime.
    Similarly, a timer thread was added to actions.py to use it to alert
    the user when a pill is due for administration.

---
+++ projects/hospital-system/actions.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+from threading import Timer
+
+import pyupm_grove as grove
+import pyupm_i2clcd as lcd
+import pyupm_ttp223 as ttp
+
+# Pins used in Grove kit.
+PILL_BUTTON = 2
+EMERGENCY_BUTTON = 3
+ALERT_LED = 4
+
+# Configuration values.
+PILL_TIMER_LIMIT = 5    # in seconds
+LCD_BACKGROUND_COLOR = (50, 50, 100)  # in RGB decimal values
+LCD_BACKGROUND_ALERT_COLOR = (255, 0, 0)    # in RGB decimal values
+
+# Alarm status.
+ON = True
+OFF = False
+alarm_status_list = {ON: "on",
+                     OFF: "off"}
+
+# Succes code for Bluetooth commands.
+SUCCESS_CODE = "OK"
+
+# Initialize your devices.
+display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+alarm_led = grove.GroveLed(ALERT_LED)
+pill_button = grove.GroveButton(PILL_BUTTON)
+emergency_button = ttp.TTP223(EMERGENCY_BUTTON)
+
+# Initialize flags.
+initialized = False
+pill_counter = 0
+pill_is_due = False
+pill_button_released = True
+emergency_button_released = True
+alarm_status = OFF
+
+# Initialize variables.
+timer = None
+
+
+def displayInit(color):
+    display.setColor(*color)
+    display.clear()
+
+
+def displayMessage(message, line, pos=0):
+    display.setCursor(line, pos)
+    display.write(message)
+
+
+def updateDisplay():
+    if not pill_is_due:
+        displayInit(LCD_BACKGROUND_COLOR)
+    else:
+        displayInit(LCD_BACKGROUND_ALERT_COLOR)
+        displayMessage("*ADMINISTER PILL", 1, 0)
+    displayMessage("Pills taken: %s" % str(pill_counter), 0, 0)
+
+
+def administerPill():
+    global pill_counter
+    pill_counter += 1
+    setNextPillTimer()
+    updateDisplay()
+    return str(SUCCESS_CODE)
+
+
+def toggleAlarm(alarmed):
+    global alarm_status
+
+    if alarmed:
+        alarm_led.on()
+        alarm_status = ON
+    else:
+        alarm_led.off()
+        alarm_status = OFF
+
+
+def resetCounter():
+    global pill_counter
+    pill_counter = 0
+    updateDisplay()
+    return str(SUCCESS_CODE)
+
+
+def activateAlarm():
+    toggleAlarm(ON)
+    return str(SUCCESS_CODE)
+
+
+def resetAlarm():
+    toggleAlarm(OFF)
+    return str(SUCCESS_CODE)
+
+
+def checkButtonStatus():
+    global pill_button_released
+    global emergency_button_released
+
+    if pill_button.value():
+        if pill_button_released:
+            administerPill()
+            # avoid input until button is released
+            pill_button_released = False
+    else:
+        # wait for new input
+        pill_button_released = True
+
+    if emergency_button.value():
+        if emergency_button_released:
+            toggleAlarm(ON)
+            # avoid input until button is released
+            emergency_button_released = False
+    else:
+        # wait for new input
+        emergency_button_released = True
+
+
+def setPillIsDue():
+    global pill_is_due
+    pill_is_due = True
+    updateDisplay()
+
+
+def setNextPillTimer():
+    global pill_is_due
+    global timer
+    if timer:
+        timer.cancel()
+    timer = Timer(PILL_TIMER_LIMIT, setPillIsDue, ())
+    timer.start()
+    pill_is_due = False
+
+
+def callFunction(fnc_id):
+    func = btFunctionList.get(fnc_id, dummy)
+    return func()
+
+
+def dummy():
+    pass
+
+
+def getPillCounter():
+    return str(pill_counter)
+
+
+def getAlarmStatus():
+    return alarm_status_list[alarm_status]
+
+
+def getPillIsDueState():
+    return str(pill_is_due)
+
+
+btFunctionList = {'a': administerPill,
+                  'b': activateAlarm,
+                  'c': resetCounter,
+                  'd': resetAlarm,
+                  'e': getPillCounter,
+                  'f': getAlarmStatus,
+                  'g': getPillIsDueState}
+
+
+def myProgram():
+    global initialized
+
+    if not initialized:
+        displayInit(LCD_BACKGROUND_COLOR)
+        updateDisplay()
+        setNextPillTimer()
+        initialized = True
+
+    checkButtonStatus()
+
+
+if __name__ == "__main__":
+    while True:
+        myProgram()
