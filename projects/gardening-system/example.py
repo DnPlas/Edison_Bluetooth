174c36ee06263eb9194529ff5fa023bdeb37fc8b
Daniela A. Plascencia <daplascen@gmail.com>
2016-11-15 04:05:44 -0600

    Modifies scripts
    Signed-off-by: Daniela A. Plascencia <daplascen@gmail.com>

--- projects/gardening-system/example.py
+++ projects/gardening-system/example.py
@@ -5,7 +5,6 @@
 # UPM Modules
 import pyupm_i2clcd as lcd
 import pyupm_grove as g
-
 from time import sleep
 
 # BT Needed Modules
@@ -14,6 +13,7 @@ import spp as s
 # ========= * SETTING UP GPIOS * ========= #
 light = g.GroveLight(0)                 # Light sensor is connected to slot A0
 display = lcd.Jhd1313m1(0, 0x3E, 0x62)  # LCD goes on any I2C slot
+relay = g.GroveRelay(4)                 # Relay module is connected to slot D4
 
 # ========= * PROJECT FUNCTIONS * ======== #
 
@@ -26,8 +26,33 @@ def displayLightInfo():
     display.write('Light:%s' % str(light.value()))  # Display light sensor data
     sleep(1)
 
+# Display message on LCD
+def displayStatus(message):    
+    display.clear()            # Clear LCD
+    display.setColor(0,0,255)  # Set LCD backlight color to blue
+    display.setCursor(0,2)     # Set cursor position (bottom left corner)
+    display.write(message)     # Display message
+
+# Change LCD backlight color based on light threshold
+def displayLight():
+    if light.value() <= 20:             # Set a low light threshold
+        display.setColor(255,0,0)       # If low light, set LCD color to RED
+    elif light.value() in range(21,29): # Set medium amount of light threshold
+        display.setColor(255,255,0)     # If mid light, set LCD color to YELLOW
+    elif light.value() >= 30:           # Set a high amount of light threshold
+        display.setColor(0,255,0)       # If high light, set LCD color to GREEN
+
 #-------------  BT Functions -------------  #
     
+# Water your plant via BT
+def waterPlant():              
+    displayStatus('Watering')     # Display message when plant is being watered
+    relay.off()                   # Turns relay off
+    sleep(1)                      
+    relay.on()                    # Turns relay on for five seconds
+    sleep(5)
+    relay.off()                   # Tunrs relay off again
+
 # Request light sensor data via BT
 def requestData():
     return str(light.value())     # Returns light sensor data
@@ -42,13 +67,14 @@ def function (data):
 def dummy(): # If a character that's not defined in 'functionInUse'
     pass     # is sent to the system via BT, it won't do a thing
 
-functionInUse = {'a' : requestData,}    # Sets which letter corresponds
-                                        # to each BT controlled function
+functionInUse = {'a' : waterPlant,    # Sets which letter corresponds
+                 'b' : requestData, } # to each BT controlled function
 
 # ------------ Project Program ------------ #
 # Run all non-BT controlled functions
 def myProgram():
-      displayLightInfo()     # Run these functions until a BT event is triggered
+      displayLight()     # Run these functions until a BT event is triggered
+      displayLightInfo()
 
 # =========== * BT MAIN LOOP * ============ #
 if __name__ == '__main__':
